2d6f29d6ce98165be943a0b34fada8bee7f8455d
Ryan Machan <rjm21@case.edu>
2017-11-29 16:49:09 -0500

    javascript pseudocode/ideas

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,11 +8,50 @@ var config = {
 	messagingSenderId: '307263825343'
 };
 firebase.initializeApp(config);
-
 var database = firebase.database();
+var playersRef = database.ref('/players');
 
-database.ref().on('value', function(snapshot) {
-	
-}, function(errorObject) {
-	console.log('The read failed: ' + errorObject.code);
+var playerNum;
+
+$('#start').on('click', function(event) {
+	event.preventDefault();
+	//maybe put code below in here?
+});
+
+// When the client's connection state changes...
+database.ref('.info/connected').on('value', function(snapshot) {
+	var player;
+	//if user is connected...
+	if (snapshot.val() === true) {
+		if(playersRef.hasChild('1')) {
+			player = playersRef.push({
+				2: {
+					name: 'Testing',
+					wins: 0,
+					losses: 0
+				}
+			});
+			playerNum = 2;
+		} else {
+			player = playersRef.push({
+				1: {
+					name: 'Testing',
+					wins: 0,
+					losses: 0
+				}
+			});
+			playerNum = 1;
+		}
+		//remove connection if user disconnects
+		player.onDisconnect().remove();
+	}
+});
+
+//TODO: when connections list changes, do stuff
+playersRef.on('value', function(snapshot) {
+	if (snapshot.numChildren() < 2) {
+		// we have an open spot
+	} else {
+		// we're ready to play
+	}
 });
