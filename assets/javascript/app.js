6d9ef3a0307c4dd24afc05fdc682fd5c5275d7b1
Ryan Machan <rjm21@case.edu>
2017-11-30 17:35:07 -0500

    add stats for players

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,8 +23,27 @@ var game = {
 	wins: 0,
 	losses: 0,
 	ties: 0,
+	oppWins: 0,
+	oppLosses: 0,
+	oppTies: 0,
 	winState: '',
 	initGame: function() {
+		//load other player's current stats (if there is another player logged in)
+		player1Ref.once('value', function(snapshot) {
+			if(snapshot.val() != null) {
+				$('#player-1-wins').text(snapshot.val().wins);
+				$('#player-1-losses').text(snapshot.val().losses);
+				$('#player-1-ties').text(snapshot.val().ties);
+			}
+		});
+		player2Ref.once('value', function(snapshot) {
+			if(snapshot.val() != null) {
+				$('#player-2-wins').text(snapshot.val().wins);
+				$('#player-2-losses').text(snapshot.val().losses);
+				$('#player-2-ties').text(snapshot.val().ties);
+			}
+		});
+
 		database.ref().on('value', function(snapshot) {
 			if (snapshot.child('players').numChildren() === 2) {
 				if(!snapshot.child('turn').exists()) {
@@ -49,6 +68,15 @@ var game = {
 					});
 				}
 
+				// reset opponent stats
+				game.oppWins = 0;
+				game.oppLosses = 0;
+				game.oppTies = 0;
+				$('#player-' + game.opponentNum + '-wins').text(game.oppWins);
+				$('#player-' + game.opponentNum + '-losses').text(game.oppLosses);
+				$('#player-' + game.opponentNum + '-ties').text(game.oppTies);
+
+				// hide player's buttons until another player enters game
 				$('#player-' + game.playerNum + '-buttons').addClass('hidden');
 			}
 
@@ -105,9 +133,23 @@ var game = {
 						if(players.hasChild('1')) {
 							game.playerNum = '2';
 							game.opponentNum = '1';
+							player1Ref.once('value', function(p1Snap) {
+								if(p1Snap.val() != null) {
+									game.oppWins = p1Snap.val().wins;
+									game.oppLosses = p1Snap.val().losses;
+									game.oppTies = p1Snap.val().ties;
+								}
+							});
 						} else {
 							game.playerNum = '1';
 							game.opponentNum = '2';
+							player2Ref.once('value', function(p2Snap) {
+								if(p2Snap.val() != null) {
+									game.oppWins = p2Snap.val().wins;
+									game.oppLosses = p2Snap.val().losses;
+									game.oppTies = p2Snap.val().ties;
+								}
+							});
 						}
 
 						// initialize new player object in db
@@ -117,6 +159,7 @@ var game = {
 							losses: 0,
 							ties: 0
 						});
+
 						$('#loginForm').hide();
 						$('#playerInfo').removeClass('hidden')
 							.text('Hi, ' + game.playerName + '! You are player ' + game.playerNum + '.');					
@@ -210,35 +253,56 @@ var game = {
 		//update wins/losses/ties for player
 		if (this.winState == 'win') {
 			this.wins++;
-			playersRef.child(this.playerNum).update({
-				wins: this.wins
-			});
+			this.oppLosses++;
 			$('#results').text($('#player-' + this.playerNum + '-name').text() + ' wins!')
 				.show();
 		}
 		else  if (this.winState == 'lose') {
 			this.losses++;
-			playersRef.child(this.playerNum).update({
-				losses: this.losses
-			});
+			this.oppWins++;
 			$('#results').text($('#player-' + this.opponentNum + '-name').text() + ' wins!')
 				.show();
 		}
 		else {
 			this.ties++;
-			playersRef.child(this.playerNum).update({
-				ties: this.ties
-			});
+			this.oppTies++;
 			$('#results').text('You tied!')
 				.show();
 		}
 
+		playersRef.child(this.playerNum).update({
+			wins: this.wins,
+			losses: this.losses,
+			ties: this.ties
+		}, function(error) {
+			if (!error) {
+				game.updateStatsDisplay(game.playerNum);
+			}
+		});
+		
 		setTimeout(function() {
 			database.ref().update({
 				turn: 1
 			});
 			$('#results').hide();
 		}, 3000);
+	},
+	updateStatsDisplay: function(playerNum) {
+		if (playerNum === '1') {
+			$('#player-1-wins').text(game.wins);
+			$('#player-1-losses').text(game.losses);
+			$('#player-1-ties').text(game.ties);
+			$('#player-2-wins').text(game.oppWins);
+			$('#player-2-losses').text(game.oppLosses);
+			$('#player-2-ties').text(game.oppTies);
+		} else {
+			$('#player-2-wins').text(game.wins);
+			$('#player-2-losses').text(game.losses);
+			$('#player-2-ties').text(game.ties);
+			$('#player-1-wins').text(game.oppWins);
+			$('#player-1-losses').text(game.oppLosses);
+			$('#player-1-ties').text(game.oppTies);
+		}
 	}
 };
 
