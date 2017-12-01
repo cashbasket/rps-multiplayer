b2005d4cb281d776f0e147c175650b71be038439
Ryan Machan <rjm21@case.edu>
2017-12-01 00:20:32 -0500

    add chat functionality and other display elements

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,6 +13,7 @@ var playersRef = database.ref('/players');
 var player1Ref = database.ref('/players/1');
 var player2Ref = database.ref('/players/2');
 var turnRef = database.ref('/turn');
+var chatRef = database.ref('/chat');
 
 var game = {
 	playerNum: '',
@@ -49,11 +50,15 @@ var game = {
 				if(!snapshot.child('turn').exists()) {
 					database.ref().child('turn').set(1);
 				}
+				$('#chat').removeClass('hidden');
 			} else {
+				$('#status').empty().addClass('hidden');
+				$('#chat').addClass('hidden');
+				$('#chatDisplay').empty();
+
 				if (snapshot.child('turn').exists()) {
 					database.ref().child('turn').remove();
 				}
-
 				if(game.playerNum === '1') {
 					player1Ref.once('value', function(snap) {
 						if (snap.val()) {
@@ -110,15 +115,36 @@ var game = {
 		// 0 = both players have taken a turn
 		turnRef.on('value', function(turnSnap) {
 			if(turnSnap.val() != 0) {
+				$('#status').removeClass('hidden');
+				if(game.playerNum === '1') {
+					if(turnSnap.val() === 1) {
+						$('#status').text('It\'s your turn!');
+					} else {
+						$('#status').text('Waiting for ' + $('#player-2-name').text() + ' to choose.');
+					}
+				} else if (game.playerNum === '2') {
+					if(turnSnap.val() === 2) {
+						$('#status').text('It\'s your turn!');
+					} else {
+						$('#status').text('Waiting for ' + $('#player-1-name').text() + ' to choose.');
+					}
+				}
 				game.playerReady(turnSnap.val());
 			}
 			else {
+				$('#status').addClass('hidden');
 				playersRef.once('value', function(snap) {
 					if(snap.numChildren() === 2) {
 						//we have two players logged in, so it's safe to process
 						game.processSelections();
 					}
 				});
+			}
+		});
+
+		chatRef.orderByChild('timestamp').limitToLast(1).on('child_added', function(msgSnap) {
+			if(msgSnap.val()) {
+				$('#chatDisplay').append('<strong>' + msgSnap.val().sender + '</strong> (' +  moment.unix(msgSnap.val().timestamp).format('hh:mm:ss A') + '): ' + msgSnap.val().message + '<br />');
 			}
 		});
 	},
@@ -186,6 +212,8 @@ var game = {
 	},
 	addSelection: function(selection) {
 		this.playerChoice = selection;
+		$('#player-' + this.playerNum + '-choice').removeClass('hidden')
+			.text(this.playerChoice);
 
 		// set player's choice in database
 		playersRef.child(this.playerNum).update({choice: selection}, function(error) {
@@ -217,32 +245,35 @@ var game = {
 				game.opponentChoice = snap.val().choice;
 			});
 		}
+		//since round is over, show what opponent chose
+		$('#player-' + this.opponentNum + '-choice').removeClass('hidden')
+			.text(this.opponentChoice);
 
-		if (this.playerChoice === 'rock') {
-			if(this.opponentChoice == 'paper') {
+		if (this.playerChoice === 'Rock') {
+			if(this.opponentChoice == 'Paper') {
 				this.winState = 'lose';
 			}
-			else if(this.opponentChoice == 'scissors') {
+			else if(this.opponentChoice == 'Scissors') {
 				this.winState = 'win';
 			}
 			else {
 				this.winState = 'tie';
 			}
-		} else if (this.playerChoice === 'paper') {
-			if(this.opponentChoice == 'scissors') {
+		} else if (this.playerChoice === 'Paper') {
+			if(this.opponentChoice == 'Scissors') {
 				this.winState = 'lose';
 			}
-			else if(this.opponentChoice == 'rock') {
+			else if(this.opponentChoice == 'Rock') {
 				this.winState = 'win';
 			}
 			else {
 				this.winState = 'tie';
 			}
-		} else if (this.playerChoice === 'scissors') {
-			if(this.opponentChoice == 'rock') {
+		} else if (this.playerChoice === 'Scissors') {
+			if(this.opponentChoice == 'Rock') {
 				this.winState = 'lose';
 			}
-			else if(this.opponentChoice == 'paper') {
+			else if(this.opponentChoice == 'Paper') {
 				this.winState = 'win';
 			}
 			else {
@@ -285,6 +316,7 @@ var game = {
 				turn: 1
 			});
 			$('#results').hide();
+			$('#player-1-choice, #player-2-choice').empty().addClass('hidden');
 		}, 3000);
 	},
 	updateStatsDisplay: function(playerNum) {
@@ -303,6 +335,18 @@ var game = {
 			$('#player-1-losses').text(game.oppLosses);
 			$('#player-1-ties').text(game.oppTies);
 		}
+	},
+	sendMessage: function(message) {
+		var tmp = $('<div>');
+		tmp.text(message);
+		var strippedMessage = tmp.text();
+
+		var chatMessage = {
+			sender: this.playerName,
+			message: strippedMessage,
+			timestamp: moment().format('X')
+		};
+		chatRef.push(chatMessage);
 	}
 };
 
@@ -317,4 +361,10 @@ $(document).ready(function() {
 		$(this).parent('div').addClass('hidden');
 		game.addSelection($(this).data('value'));
 	});
+
+	$('#msgForm').on('submit', function(event) {
+		event.preventDefault();
+		game.sendMessage($('#message').val().trim());
+		$('#message').val('');
+	})
 });
